a6da9021ab7842d391e3f861d3a0af021365b60d
zepher2211 <zepher2211@gmail.com>
2018-08-16 20:08:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments) {
+  var emptyBucket = [];
+  for (let i = 0; i < musicians.length; i++) {
+    emptyBucket.push(`${musicians[i]} plays ${instruments[i]}`);
+    }
+    return emptyBucket;
+}
+function johnLennonFacts(facts) {
+  var n = 0
+  while (n < facts.length) {
+    n++ ;
+    facts.push(`${facts[n]} !!!`)
+  }
+  return facts;
+}
